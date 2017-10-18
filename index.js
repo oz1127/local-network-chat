6d39ec657a79262f9a9b818b2b99495dde4610ba
Jannis R <mail@jannisr.de>
2017-10-18 21:48:09 +0200

    render chat messages with colors

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict'
 
 var createChannel = require('multicast-channel')
+var chalk = require('chalk')
 
 // channel is an event emitter
 const channel = createChannel({name: 'derhuerst'})
 
 channel.on('error', console.error)
 channel.on('message', (msg, from) => {
-	console.log('got a message!', msg, from)
+	console.log(chalk.red(from) + chalk.yellow(msg))
 })
 
 channel.on('open', () => {
 	channel.send({hello: 'friends!'})
 })
-
