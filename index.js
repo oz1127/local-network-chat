57afbe28b04da5bd4da52b8f270db893a2be2f66
workdev-ho <ozdemir1127@gmail.com>
2017-10-18 21:22:50 +0200

    added index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 'use strict'
 
+var createChannel = require('multicast-channel')
+
+// channel is an event emitter
+const channel = createChannel({name: 'derhuerst'})
+
+channel.on('error', console.error)
+channel.on('message', (msg, from) => {
+	console.log('got a message!', msg, from)
+})
+
+channel.on('open', () => {
+	channel.send({hello: 'friends!'})
+})
+
