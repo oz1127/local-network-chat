c1ca9085823a61346d85a1f2196be57e1b080365
workdev-ho <ozdemir1127@gmail.com>
2017-10-18 21:46:19 +0200

    updated index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict'
 
 var createChannel = require('multicast-channel')
+var randomWord = require('random-word')
 
 // channel is an event emitter
-const channel = createChannel({name: 'derhuerst'})
+const channel = createChannel({name: process.argv[2]})
 
 channel.on('error', console.error)
 channel.on('message', (msg, from) => {
@@ -11,6 +12,10 @@ channel.on('message', (msg, from) => {
 })
 
 channel.on('open', () => {
-	channel.send({hello: 'friends!'})
+	setInterval(function(){
+		//send random word
+		channel.send(randomWord())
+		//channel.send({hello: 'friends!'})
+	}, 2000)
 })
 
